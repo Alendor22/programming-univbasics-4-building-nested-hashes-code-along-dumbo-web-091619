e1bd759eb5df319d9c3094d8d8d150465fee12b3
Adeja Lendor <alendor6@gmail.com>
2019-08-29 16:07:31 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,6 +21,10 @@ def bonus
   }
 
 epic_tragedy[:hero][:status] = "dead"
+epic_tragedy[:heroine][:status] = ""
+puts epic_tragedy
+
+
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
