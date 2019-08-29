166d09a2bfa3cf7c81857680063b6831c012aa4b
Adeja Lendor <alendor6@gmail.com>
2019-08-29 15:33:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,16 +3,13 @@ def third_challenge
    :montague => {
       :patriarch => {:name => "Lord Montague", :age => "53"},
       :matriarch => {:name => "Lady Montague", :age => "54"},
-      :hero => {
-        },
+      :hero => {:name => "Romeo", :age => "15", :status => "alive"},
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
+      :patriarch => {:name => "Lord Capulet", :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
         },
       :heroine => {
         #your key/value pairs here
